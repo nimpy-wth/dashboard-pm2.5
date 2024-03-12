171f2acaf222811603684ca9bee6d93d56297ee5
nimpy-wth <nimmyliala@gmail.com>
2024-03-12 13:14:22 +0700

    add navbar

--- web.py
+++ web.py
@@ -6,7 +6,7 @@ import pandas as pd
 import plotly.express as px
 
 data = pd.read_csv("cleaned_data.csv")
-predict_data = pd.read_csv("hatyai.csv")
+predict_data = pd.read_csv("train.csv")
 data["DATETIMEDATA"] = pd.to_datetime(data["DATETIMEDATA"], format="%Y-%m-%d %H:%M:%S")
 data.sort_values("DATETIMEDATA", inplace=True)
 
@@ -17,12 +17,27 @@ external_stylesheets = [
         "rel": "stylesheet",
     },
 ]
+
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title = "Air Quality Analytics: Understand Air Quality!"
 
-app.layout = html.Div(
+navbar = html.Div(
+    className="navbar",  # Added a class name for styling
     children=[
+        html.Nav(
+            className="nav",
+            children=[
+                html.A('Home', href='/'),
+                html.A('Prediction', href='/prediction'),
+            ]
+        )
+    ]
+)
+
+home_layout = html.Div(
+    children=[
+        navbar,
         html.Div(
             children=[
                 html.P(children="🌞", className="header-emoji"),
@@ -106,39 +121,6 @@ app.layout = html.Div(
                 ),
                     className="card",
                     style={"width": "50%", "float": "left"},  # Add this line
-                ),
-            ],
-            className="wrapper",
-        ),
-
-
-        html.Div(
-            children=[
-                html.Div(
-                    children=[
-                        html.Div(children="Parameter", className="menu-title"),
-                        dcc.Dropdown(
-                            id="parameter-predict",
-                            options=[
-                                {"label": param, "value": param}
-                                for param in predict_data.columns[1:]
-                            ],
-                            value="PM25",
-                            clearable=False,
-                            className="dropdown",
-                        ),
-                    ]
-                ),
-            ],
-            className="menu",
-        ),
-        html.Div(
-            children=[
-                html.Div(
-                    children=dcc.Graph(
-                        id="predict-chart", config={"displayModeBar": False},
-                    ),
-                    className="card",
                 ),
             ],
             className="wrapper",
@@ -230,6 +212,57 @@ def update_stats_table(selected_parameter, start_date, end_date):
 
 ############################################# PREDICTION GRAPH #############################################
 
+predict_page = html.Div(
+    children=[
+        navbar,
+        html.Div(
+            children=[
+                html.P(children="🌞", className="header-emoji"),
+                html.H1(
+                    children="Air Quality Analytics", className="header-title"
+                ),
+                html.P(
+                    children="Analyze the air quality data",
+                    className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(children="Parameter", className="menu-title"),
+                        dcc.Dropdown(
+                            id="parameter-predict",
+                            options=[
+                                {"label": param, "value": param}
+                                for param in predict_data.columns[1:]
+                            ],
+                            value="PM25",
+                            clearable=False,
+                            className="dropdown",
+                        ),
+                    ]
+                ),
+            ],
+            className="menu",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="predict-chart", config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
+        ),
+    ]
+)
+
 @app.callback(
     Output("predict-chart", "figure"),
     [
@@ -257,6 +290,23 @@ def update_predict_chart(selected_parameter, start_date, end_date):
         "colorway": ["#579AF5"],  # or any other color
     }
     return {"data": [trace], "layout": layout}
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content'),
+    dcc.Interval(id='interval-component',interval=60000)
+    ]
+)
+
+@app.callback(Output('page-content', 'children'), Input('url', 'pathname'))
+def display_page(pathname):
+    if pathname == '/':
+        return home_layout
+    elif pathname == '/prediction':
+        return predict_page
+    else:
+        return '404 Page Not Found'
+    
 
 if __name__ == "__main__":
     app.run_server(debug=True)
